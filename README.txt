e79b6de073336a25898adbbffe27c3e73953c4ce
Marshall Perrin <mperrin@stsci.edu>
2014-12-06 07:08:39 +0000

    gpipy: Add setup.py

---
+++ README.txt
@@ -0,0 +1,16 @@
+Usage: 
+
+
+% python setup.py install
+   OR
+% python setup.py build
+
+Use the latter if you want edits in this package to be immediately available to
+your Python sessions without having to reinstall.
+
+
+
+Then start python and:
+
+>>> import gpipy
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='gpipy',
+      version='0.1',
+      description='Gemini Planet Imager Python utilities',
+      author='GPI team',
+      author_email='planetimager@gmail.com',
+      url='http://www.planetimager.org',
+      packages=['gpipy'],
+     )
